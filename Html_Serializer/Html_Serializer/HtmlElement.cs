e990795ca95fc9379385af01da7f3e0c98934c68
Noa-Goldfinger <you@example.com>
2024-02-18 12:05:20 +0200

    init proj

---
+++ Html_Serializer/Html_Serializer/HtmlElement.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Html_Serializer
+{
+    public class HtmlElement
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Attributes { get; set; }
+        public List<string> Classes { get; set; }
+        public string InnerHtml { get; set; }
+        public HtmlElement Parent { get; set; }
+        public List<HtmlElement> Children { get; set; }
+        public HtmlElement()
+        {
+            Attributes = new List<string>();
+            Classes = new List<string>();
+            Children = new List<HtmlElement>();
+        }
+        public IEnumerable<HtmlElement> Descendants()
+        {
+            var queue = new Queue<HtmlElement>();
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                var currentElement = queue.Dequeue();
+                yield return currentElement;
+
+                foreach (var child in currentElement.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+        }
+        public IEnumerable<HtmlElement> Ancestors()
+        {
+            var currentElement = this;
+            while (currentElement != null)
+            {
+                yield return currentElement;
+                currentElement = currentElement.Parent;
+            }
+        }
+        public  string ToString2()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"<{Name}");
+            if (!string.IsNullOrEmpty(Id))
+            {
+                sb.Append($" id=\"{Id}\"");
+            }
+            if (Classes.Any())
+            {
+                sb.Append(" class=\"");
+                sb.Append(string.Join(" ", Classes));
+                sb.Append("\"");
+            }
+            if (Attributes.Any())
+            {    sb.Append(" attribute :");
+
+            foreach (var attribute in Attributes)
+            {
+                sb.Append($" {attribute}");
+            }
+            }
+            sb.Append(">");
+            if (!string.IsNullOrEmpty(InnerHtml))
+            {
+                sb.Append(InnerHtml);
+            }
+
+            sb.Append($"</{Name}>");
+            sb.Append("count of children elements: "+Children.Count());
+
+            return sb.ToString();
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"name: {Name}");
+
+            if (Classes.Any())
+            {
+                sb.AppendLine($"classes: {string.Join(" ", Classes)}");
+            }
+
+            if (Attributes.Any())
+            {
+                sb.AppendLine("attributes:");
+                foreach (var attribute in Attributes)
+                {
+                    sb.AppendLine($"- {attribute}");
+                }
+            }
+
+            if (!string.IsNullOrEmpty(InnerHtml))
+            {
+                sb.AppendLine(InnerHtml);
+            }
+
+            sb.AppendLine($"count of children elements: {Children.Count}");
+
+            return sb.ToString();
+        }
+    }    
+     public static class HtmlElementExtensions
+    {
+        public static HashSet<HtmlElement> GetElementsBySelector(this HtmlElement element, Selector selector)
+        {
+            var result = new HashSet<HtmlElement>();
+            var descendants = element.Descendants();
+
+            foreach (var descendant in descendants) 
+            { if (CheckingElementMatchesSelector(selector, descendant))
+                {
+                    List<HtmlElement> listParent = descendant.Ancestors().ToList();
+                    if (MatchesSelectorRecursive(listParent,1, selector.Parent))
+                        result.Add(descendant);
+                }
+            }
+            return result;
+        }
+        private static bool MatchesSelectorRecursive(List<HtmlElement> listParent,int index, Selector selector)
+        {
+            if (index == listParent.Count())
+                return false;
+            if (CheckingElementMatchesSelector(selector, listParent[index]))
+            {
+                if (selector.Parent == null)
+                    return true;
+                return MatchesSelectorRecursive(listParent,index+1 ,selector.Parent);
+            }
+                return MatchesSelectorRecursive(listParent,index+1, selector); 
+        }
+
+        private static bool CheckingElementMatchesSelector(Selector selector, HtmlElement parent)
+        {
+            if (!string.IsNullOrEmpty(selector.TagName) && parent.Name != selector.TagName)
+                return false;
+
+            if (!string.IsNullOrEmpty(selector.Id) && parent.Id != selector.Id)
+                return false;
+
+            if (selector.Classes != null)
+            {
+                foreach (var className in selector.Classes)
+                {
+                    if (!parent.Classes.Contains(className))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+
+}
+
+
+
---
+++ Html_Serializer/Html_Serializer/Selector.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+using Html_Serializer;
+
+namespace Html_Serializer
+{
+
+    public class Selector
+    {
+        public string TagName { get; set; }
+        public string Id { get; set; }
+        public List<string> Classes { get; set; }
+        public Selector Parent { get; set; }
+        public Selector Child { get; set; }
+
+        public Selector()
+        {
+            Classes = new List<string>();
+        }
+        public static Selector FromQueryString(string query)
+        {
+            string[] selectorStrings = query.Split(" ");
+            Selector rootSelector = null;
+            Selector currentSelector = null;
+            string[] validHtmlTags = HtmlHelper.Instance.SelfClosingTags.Concat(HtmlHelper.Instance.AllTags).ToArray();
+
+
+            foreach (string selectorString in selectorStrings)
+            {
+                Selector selector = new Selector();
+                string[] parts = selectorString.Split(new[] { '#', '.' }, StringSplitOptions.RemoveEmptyEntries);
+                int indexof1 = selectorString.IndexOf('#');
+
+                string hash = "";
+                if (indexof1 != -1)
+                {
+                    int indexof2 = selectorString.IndexOf('.', indexof1);
+
+                    if (indexof2 == -1)
+                        hash = selectorString.Substring(indexof1 + 1);
+                    else hash = selectorString.Substring(indexof1 + 1, indexof2 - 1 - indexof1);
+                }
+                int fromNum = 0;
+                if (selectorString[0] != '.' && selectorString[0] != '#' && IsValidHtmlTagName(parts[0], validHtmlTags))
+                {
+                    selector.TagName = parts[0];
+                    fromNum = 1;
+                }
+                if (hash!.Length > 0)
+                    selector.Id = hash;
+                for (int i = fromNum; i < parts.Length ; i++)
+                {
+                    if (parts[i] != hash)
+                        selector.Classes.Add(parts[i]);
+
+                }
+                if (currentSelector != null)
+                {
+                    selector.Parent = currentSelector;
+                    currentSelector.Child = selector;
+                }
+
+                if (rootSelector == null)
+                {
+                    rootSelector = selector;
+                }
+
+                currentSelector = selector;
+            }
+
+            return currentSelector;
+        }
+
+    
+
+
+
+
+
+
+
+private static bool IsValidHtmlTagName(string tagName, string[] validHtmlTags)
+        {
+            return validHtmlTags.Contains(tagName, StringComparer.OrdinalIgnoreCase);
+        }
+    
+
+    public override string ToString()
+        {
+            string result = $"Tag Name: {TagName}\n";
+            result += $"Id: {Id}\n";
+            result += "Classes: ";
+            if (Classes != null)
+            {
+                result += string.Join(", ", Classes);
+            }
+            result += "\n";
+
+            return result;
+            
+        }
+
+    }
+}
